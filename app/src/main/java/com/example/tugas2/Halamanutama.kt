22d9d576f53b72abac9919202498e1b4f6d0b2b3
HeruAdhiSaputra <22421013.heru@gmail.com>
2025-03-24 17:14:18 +0900

    baru

--- app/src/main/java/com/example/tugas2/Halamanutama.kt
+++ app/src/main/java/com/example/tugas2/Halamanutama.kt
@@ -1,30 +1,27 @@
 package com.example.tugas2
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.tugas2.databinding.ActivityHalamanutamaBinding
 import com.example.tugas2.databinding.ActivityLupapasswordBinding
-import com.example.tugas2.databinding.ActivityMainBinding
 
-class Lupapassword : AppCompatActivity() {
-    private lateinit var binding: ActivityLupapasswordBinding
+class Halamanutama : AppCompatActivity() {
+    private lateinit var binding: ActivityHalamanutamaBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_lupapassword)
+        setContentView(R.layout.activity_halamanutama)
 //        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
 //            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
 //            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
 //            insets
 //        }
-        binding = ActivityLupapasswordBinding.inflate(layoutInflater)
+        binding = ActivityHalamanutamaBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.kembali2.setOnClickListener{
-            val intentHome = Intent(this,MainActivity::class.java)
-            startActivity(intentHome) }
+
     }
 }
--- app/src/main/java/com/example/tugas2/Register2.kt
+++ app/src/main/java/com/example/tugas2/Register2.kt
@@ -1,20 +1,41 @@
 package com.example.tugas2
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.tugas2.databinding.ActivityRegister2Binding
 
 class Register2 : AppCompatActivity() {
+    private lateinit var binding: ActivityRegister2Binding
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_register2)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+//        }
+        setContentView(R.layout.activity_register2)
+        binding = ActivityRegister2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.kembali1.setOnClickListener{
+            val intentHome = Intent(this,MainActivity::class.java)
+            startActivity(intentHome) }
+
+        binding.register.setOnClickListener{
+            val intentHome = Intent(this,MainActivity::class.java)
+            startActivity(intentHome) }
+
+
+
+
+
+
     }
 }
